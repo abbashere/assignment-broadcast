63af1d474060f3319089c89f3f7b4f432ab4d153
ahamad khan <ahamad.khan@allianz.co.id>
2022-02-12 14:39:51 +0530

    added all file and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import { FirstComponent } from './first.component';
+import { SecondComponent } from './second.component';
+
+import { SharedService } from './component.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, FirstComponent, SecondComponent],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
